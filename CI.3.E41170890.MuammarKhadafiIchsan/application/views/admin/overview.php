9e2e8f918ed7f619c1f643947a0cbdb6a54f88ab
muammarkhadafiichsan <47959678+muammarkhadafiichsan@users.noreply.github.com>
2019-03-11 11:28:00 +0700

    tambahan

--- CI.3.E41170890.MuammarKhadafiIchsan/application/views/admin/overview.php
+++ CI.3.E41170890.MuammarKhadafiIchsan/application/views/admin/overview.php
@@ -12,7 +12,7 @@
     <title>SB Admin - Dashboard</title>
 
 	<!-- Bootstrap core CSS-->
-	<link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+	<link href="<?php echo base_url('css/sb-admin.min.css') ?>" rel="stylesheet">
 
 	<!-- Custom fonts for this template-->
 	<link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
