039afeaaf0b3b0f5a6016f7c33e185cb37c91c9d
Marettadwi <47960431+Marettadwi@users.noreply.github.com>
2019-03-12 14:56:35 +0700

    TugasCI

---
+++ C1.3.E41170959_P2/application/views/home.php
@@ -0,0 +1,410 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Option a Corporate Category Flat Bootstarp responsive Website Template| Home :: w3layouts</title>
+<link href="<?php echo base_url('assets/css/bootstrap.css');?>" rel='stylesheet' type='text/css'/>
+<link href="<?php echo base_url('assets/css/style.css');?>" rel="stylesheet" type="text/css" media="all"/>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,200,500,700,600,800,300,100' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="Option Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="<?php echo base_url('assets/js/jquery.min.js');?>"> </script>
+<!---- start-smoth-scrolling---->
+		<script type="text/javascript" src="<?php echo base_url('assets/css/js/move-top.js');?>"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+				});
+			});
+		</script>
+ 	<!---- start-smoth-scrolling---->
+
+</head>
+<body>
+<!--body-->
+<ul class="side_nav">
+<li><a class="scroll" href="#home"></a></li>
+<li><a class="scroll" href="#about"></a></li>
+<li><a class="scroll" href="#skills"></a></li>
+<li><a class="scroll" href="#gallery"></a></li>
+<li><a class="scroll" href="#contact"></a></li>
+</ul>
+<div id="home" class="banner">
+	 <div class="container">
+		 <div class="top-header">		  
+				<div class="logo">
+					<a href="index.html"><img src="images/logo.png" alt="" /></a>
+				</div>			  
+				 <div class="top-menu">
+					 <span class="menu"></span> 
+					 <nav class="cl-effect-1">
+				      <ul>
+						 <li><a class="scroll" href="#about">About</a></li>
+						 <li><a class="scroll" href="#skills">Skills</a></li>
+						 <li><a class="scroll" href="#gallery">Gallery</a></li>
+						 <li><a class="scroll" href="#contact">Contact</a></li>						 
+					 </ul>	
+					 </nav>					 
+				 </div>	
+				  <div class="clearfix"> </div>
+					 	 
+		 </div>	
+		  <div class="clearfix"> </div>
+		 <!-- script-for-menu -->
+		 <script>
+				$("span.menu").click(function(){
+					$(".top-menu ul").slideToggle("slow" , function(){
+					});
+				});
+		 </script>
+		 <!-- script-for-menu -->	  	 
+		  <div class="banner-info">	
+				<h1>Always Designs Something Good</h1>
+				<h3>Vivamus scelerisque elementum viverra. Donec accumsan dui quis lectus dictum, vestibulum interdum elit tempor. Donec rhoncus bibendum tempor.</h3>
+				<div class="scroll-down">
+				 <h4>SCROLL DOWN</h4>
+				<a class="downarrow scroll" href="#skills"><span></span></a>
+				</div>
+		  </div>
+	  </div>
+</div>
+<div class="social-media">
+	 <a href="#"><div class="behance social-grids">
+		 <i class="bh"></i>
+	 </div></a>
+	 <a href="#"><div class="facebook social-grids">
+		 <i class="fb"></i>
+	 </div></a>
+	 <a href="#"><div class="pinterest social-grids">
+		 <i class="pin"></i>
+	 </div></a>
+	 <a href="#"><div class="birbble social-grids">
+		 <i class="brib"></i>
+	 </div></a>
+	 <div class="clearfix"></div>
+</div>
+<!---->
+<div id="about" class="about">
+	 <div class="container">
+		 <div class="about-head">
+			 <h3>About OPTION</h3>
+			 <p>Recognizing the need is the primary condition for design.</p>
+			 <h5>Charles Eames</h5>
+		 </div> 
+		 <div class="about-sec">
+			 <div class="col-md-6 about-info">
+				 <p class="about-sec-info"><span>OPTION</span>is originally an option of mine, other ideas that pop up, when creating my design for my personal portfolio. So instead of just keeping it inside my storage files why not share it? 
+				 I'm giving away this PSD template totally free. No need for appreciate but giving me one will be great! So enjoy! and sorry if there's something wrong in my PSD layers. 
+				 Thanks to Graphic Burger for the awesome set of icons and mockups. Also thanks to Unsplash for the great images.</p>
+				 <div class="about-grid">
+					 <div class="about-icon">
+						 <i class="icon1"></i>
+					 </div>
+					 <div class="about-icon-info">
+						 <h4>Designed with Bootstrap 3.0 Grid</h4>
+						 <p>Proin sapien leo, venenatis facilisis tincidunt sit amet, vestibulum quis nibh. Pellentesque habitant morbi
+							tristique senectus et netus et malesuada fames ac turpis egestas. </p>
+					 </div>
+					 <div class="clearfix"></div>
+				 </div>
+				 <div class="about-grid">
+					 <div class="about-icon">
+						 <i class="icon2"></i>
+					 </div>
+					 <div class="about-icon-info">
+						 <h4>Easy to convert from PSD to HTML</h4>
+						 <p>Nullam mi est, bibendum sit amet diam eget, vulputate interdum erat. Etiam condimentum in ex id scelerisque.
+						 Nam porta, elit at luctus maximus, justo magna ornare purus.</p>
+					 </div>
+					 <div class="clearfix"></div>
+				 </div>
+			 </div>
+			 <div class="col-md-6 about-phones">
+				 <img src="images/phones.png" alt=""/>
+			 </div>
+			 <div class="clearfix"></div>
+		 </div>
+	 </div>
+</div>
+<!---->
+<div id="skills" class="skills">
+	 <div class="container">
+		 <div class="skills-head">
+			 <h3>SKILLS</h3>
+			 <p>Color does not add a pleasant quality to design - it reinforces it.</p>
+			 <h5>Pierre Bonnard</h5>
+		 </div> 
+		 <div class="skill-grids">
+			 <div class="col-md-4 skill-grid">
+				 <img src="images/research.png" alt=""/>
+				 <h3><a href="#">RESEARCH</a></h3>
+				 <p>Aenean tempus orci vel dolor accumsan gravida. Aliquam erat volutpat</p>
+			 </div>
+			 <div class="col-md-4 skill-grid">
+				 <img src="images/design.png" alt=""/>
+				 <h3><a href="#">DESIGN</a></h3>
+				 <p>Aenean tempus orci vel dolor accumsan gravida. Aliquam erat volutpat</p>
+			 </div>
+			 <div class="col-md-4 skill-grid">
+				 <img src="images/code.png" alt=""/>
+				 <h3><a href="#">CODE</a></h3>
+				 <p>Aenean tempus orci vel dolor accumsan gravida. Aliquam erat volutpat</p>
+			 </div>
+			 <div class="clearfix"></div>
+		 </div>
+		 <div class="pencils">
+			 <img src="images/pencils.png" alt=""/>
+		 </div>
+	 </div>
+</div>
+<!---->
+<!------ Light Box ------>
+    <link rel="stylesheet" href="css/swipebox.css">
+    <script src="js/jquery.swipebox.min.js"></script> 
+    <script type="text/javascript">
+		jQuery(function($) {
+			$(".swipebox").swipebox();
+		});
+	</script>
+    <!------ Eng Light Box ------>	
+<div id="gallery" class="gallery">
+	 <div class="container">
+		 <div class="gallery-head">
+			 <h3>GALLERY</h3>
+			 <p>The details are not the details. They make the design.</p>
+			 <h5>Charles Eames</h5>
+		 </div>
+		 </div>
+			  <ul id="filters" class="clearfix">
+						<li class="active"><span class="filter" data-filter="app card icon logos web">VIEW ALL</span></li>
+						<li><span class="filter" data-filter="app">MORE ON BEHANCE</span></li>
+			 </ul>
+					<div id="portfoliolist">
+					<div class="portfolio card mix_all  wow bounceIn" data-wow-delay="0.4s" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic1.jpg" class="swipebox"  title="Image Title"> <img src="images/pic1.jpg" class="img-responsive" alt="">
+							 <div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+					</div>				
+					<div class="portfolio app mix_all  wow bounceIn" data-wow-delay="0.4s" data-cat="app" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic2.jpg" class="swipebox"  title="Image Title"> <img src="images/pic2.jpg" class="img-responsive" alt="">
+							 <div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+					</div>					
+					<div class="portfolio icon mix_all  wow bounceIn" data-wow-delay="0.4s" data-cat="icon" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic3.jpg" class="swipebox"  title="Image Title"> <img src="images/pic3.jpg" class="img-responsive" alt="">
+							 <div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+
+					</div>
+					
+					<div class="portfolio app mix_all  wow bounceIn" data-wow-delay="0.4s" data-cat="app" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic4.jpg" class="swipebox"  title="Image Title"> <img src="images/pic4.jpg" class="img-responsive" alt="">
+							 <div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+					</div>			
+					<div class="portfolio card mix_all  wow bounceIn" data-wow-delay="0.4s" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic5.jpg" class="swipebox"  title="Image Title"> <img src="images/pic5.jpg" class="img-responsive" alt="">
+							 <div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+					</div>	
+					<div class="portfolio card mix_all  wow bounceIn" data-wow-delay="0.4s" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic6.jpg" class="swipebox"  title="Image Title"> <img src="images/pic6.jpg" class="img-responsive" alt="">
+							 <div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+					</div>	
+					<div class="portfolio icon mix_all  wow bounceIn" data-wow-delay="0.4s" data-cat="icon" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic7.jpg" class="swipebox"  title="Image Title"> <img src="images/pic7.jpg" class="img-responsive" alt="">
+								<div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+						</div>
+						<div class="portfolio logos mix_all wow bounceIn" data-wow-delay="0.4s" data-cat="logos" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper grid_box">		
+							 <a href="images/pic8.jpg" class="swipebox"  title="Image Title"> <img src="images/pic8.jpg" class="img-responsive" alt="">
+							<div class="caption">
+							 <div class="caption-info">
+								 <h4>MAURIS PHARETRA</h4>								
+								 <p>BRANDING</p>
+								 <p>PRODUCT DESIGN</p>
+								 </div>
+								 </div></a>
+		                </div>
+					</div>
+					
+<div class="clearfix"></div>					
+				</div>
+				
+	  <!-- Script for gallery Here -->
+				<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+					<script type="text/javascript">
+					$(function () {
+						
+						var filterList = {
+						
+							init: function () {
+							
+								// MixItUp plugin
+								// http://mixitup.io
+								$('#portfoliolist').mixitup({
+									targetSelector: '.portfolio',
+									filterSelector: '.filter',
+									effects: ['fade'],
+									easing: 'snap',
+									// call the hover effect
+									onMixEnd: filterList.hoverEffect()
+								});				
+							
+							},
+							
+							hoverEffect: function () {
+							
+								// Simple parallax effect
+								$('#portfoliolist .portfolio').hover(
+									function () {
+										$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+										$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+									},
+									function () {
+										$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+										$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+									}		
+								);				
+							
+							}
+				
+						};
+						
+						// Run the show!
+						filterList.init();
+						
+						
+					});	
+					</script>
+
+</div>
+<!---->
+<div id="contact" class="contact">
+	 <div class="container">
+		 <div class="contact-head">
+			 <h3>CONTACT</h3>
+			 <p>To design is to communicate clearly by whatever means you can control or master.</p>
+			 <h5>Milton Glaser</h5>
+			 </div>
+			 <div class="contact-sec">
+				 <div class="col-md-4 contact-address">					
+					 <ul>
+					 <li><span class="navi"></span></li>
+					 <li>Angono, Rizal, Philippines 1930</li>	
+					 </ul>
+					 <ul>
+					 <li><span class="mail"></span></li>
+					 <li><a href="mailto:info@example.com">yorsaquing@gmail.com</a></li>
+					 </ul>
+					 <ul>
+					 <li><span class="mbil"></span></li>
+					 <li class="number">012.3456.789</li>
+					 </ul>
+					 <div class="clearfix"></div>
+					 </ul>				 
+				 </div>
+				 <div class="col-md-8 contact-form">
+					 <div class="contect-form-left">
+						 <form>
+					     <input type="text" placeholder="Name" required/>
+						 <input type="text" placeholder="E-mail Address" required/>
+						 <input type="text" placeholder="Contact Number" required/>
+						 <input type="checkbox"/>
+						 <p class="agree">Check if you want news or freebies from me straight to your inbox</p>
+						 </form>
+					 </div>
+					 <div class="contect-form-right">
+						 <form>
+						 <textarea placeholder="Message"></textarea>
+						 <input type="submit" value="SUBMIT"/>
+						 </form>
+					 </div>
+					 <div class="clearfix"></div>
+				 </div>
+				 <div class="clearfix"></div>
+			 </div>
+		 </div>
+</div>
+<!---fotter-->
+<div class="fotter">
+	 <div class="container">
+		 <div class="fotter-logo">
+			 <a href="index.html"><img src="images/logo.png" alt=""/></a>
+		 </div>
+		 <p class="fotter-info">Copyrights &copy; 2015 Option All rights reserved | Template by <a href="http://w3layouts.com/">W3layouts</a></p>
+		 <div class="social-icons">
+			<a href="#"><span class="beh"></span></a>
+			<a href="#"><span class="face"></span></a>
+			<a href="#"><span class="pin"></span></a>
+			<a href="#"><span class="br"></span></a>				 
+		 </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+<!---->
+</body>
+</html>
