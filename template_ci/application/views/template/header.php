f6ab958b8f0b6264a32738df10a3b1d45db0f95e
egakustianpratama99 <47926994+egakustianpratama99@users.noreply.github.com>
2019-03-27 08:13:13 +0700

    routes

--- template_ci/application/views/template/header.php
+++ template_ci/application/views/template/header.php
@@ -1,9 +1,6 @@
 <ul>
-	<li style="color: white;font-size: 25px;font-weight: bold">PROPERTI.COM &raquo;</li>
-	<li><a href="<?php echo base_url('index.php/main'); ?>">HOME</a></li>
-	<li><a href="<?php echo base_url('index.php/main/berita'); ?>">JUAL</a></li>
-	<li><a href="<?php echo base_url('index.php/main/berita'); ?>">SEWA</a></li>
-	<li><a href="<?php echo base_url('index.php/main/berita'); ?>">CARI KONTRAKTOR</a></li>
-	<li><a href="<?php echo base_url('index.php/main/berita'); ?>">PENJUAL BAGUNAN</a></li>
-	<li><a href="<?php echo base_url('index.php/main/tentang'); ?>">TENTANG KAMI</a></li>
+	<li style="color: white;font-size: 25px;font-weight: bold">My Site &raquo;</li>
+	<li><a href="<?php echo base_url('index.php/main'); ?>">Home</a></li>
+	<li><a href="<?php echo base_url('index.php/main/berita'); ?>">Berita</a></li>
+	<li><a href="<?php echo base_url('index.php/main/tentang'); ?>">Tentang Kami</a></li>
 </ul>
