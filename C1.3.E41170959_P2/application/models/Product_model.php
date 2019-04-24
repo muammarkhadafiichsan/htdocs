b05ad9186e88b67e50dff17ca36fed54b49b593c
Marettadwi <47960431+Marettadwi@users.noreply.github.com>
2019-04-24 10:44:25 +0700

    uplod gambar

--- C1.3.E41170959_P2/application/models/Product_model.php
+++ C1.3.E41170959_P2/application/models/Product_model.php
@@ -43,6 +43,7 @@ class Product_model extends CI_Model
         $this->product_id = uniqid();
         $this->name = $post["name"];
         $this->price = $post["price"];
+        $this->image = $this-> _uploadImage();
         $this->description = $post["description"];
         $this->db->insert($this->_table, $this);
     }
@@ -53,6 +54,7 @@ class Product_model extends CI_Model
         $this->product_id = $post["id"];
         $this->name = $post["name"];
         $this->price = $post["price"];
+
         if (!empty($_FILES["image"]["name"])) {
             $this->image = $this->_uploadImage();
         } else {
@@ -72,25 +74,17 @@ class Product_model extends CI_Model
     $config['upload_path']          = './upload/product/';
     $config['allowed_types']        = 'gif|jpg|png';
     $config['file_name']            = $this->product_id;
-    $config['overwrite']            = true;
-    $config['max_siza']             = 1024; // 1MB
+    $config['overwrite']			= true;
+    $config['max_size']             = 1024; // 1MB
     // $config['max_width']            = 1024;
     // $config['max_height']           = 768;
 
     $this->load->library('upload', $config);
 
     if ($this->upload->do_upload('image')) {
-        return $this->upload->data("file_nama");
+        return $this->upload->data("file_name");
     }
-
-    return "default.jpg";
-}
-private function _deleteImage($id)
-{
-    $product = $this->getById($id);
-    if ($product->image != "default.jpg") {
-        $filename = explode(".", $product->image)[0];
-        return array_map('unlink', glob(FCPATH."upload/product/$filename.*"));
-    }
+    
+    return "oppa.jpg";
 }
 }
