394e5d02c59168bfcf2ed070f038e981a13222ed
egakustianpratama99 <47926994+egakustianpratama99@users.noreply.github.com>
2019-06-18 14:19:16 +0700

    baru

---
+++ android/terbaru/app/src/main/java/com/jemberdeveloper/slidingtab/DuaFragment.java
@@ -0,0 +1,63 @@
+package com.jemberdeveloper.slidingtab;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DuaFragment extends Fragment implements View.OnClickListener {
+
+    private LinearLayout forumbisnis;
+    private LinearLayout kunjunganternak;
+    private LinearLayout jualbeli;
+    public DuaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dua, container, false);
+
+        forumbisnis = (LinearLayout) view.findViewById(R.id.forumbisnis);
+        forumbisnis.setOnClickListener(this);
+
+        kunjunganternak = (LinearLayout) view.findViewById(R.id.kunjunganternak);
+        kunjunganternak.setOnClickListener(this);
+
+        jualbeli = (LinearLayout) view.findViewById(R.id.jualbeli);
+        jualbeli.setOnClickListener(this);
+
+
+        return  view;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == forumbisnis){
+            Intent intent =new Intent(getActivity(),ForumBisnis.class);
+            startActivity(intent);
+        }
+
+        if(view == kunjunganternak){
+            Intent intent =new Intent(getActivity(),KunjunganTernak.class);
+            startActivity(intent);
+        }
+        if(view == jualbeli){
+            Intent intent =new Intent(getActivity(),JualBeli.class);
+            startActivity(intent);
+        }
+    }
+}
