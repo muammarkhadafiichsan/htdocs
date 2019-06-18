ed431b54d173c962b5a37122dc2f64ab485a9665
muammarkhadafiichsan <47959678+muammarkhadafiichsan@users.noreply.github.com>
2019-06-18 19:48:24 +0700

    dinas_peternakan

--- dinas_peternakan/application/views/welcome_message.php
+++ dinas_peternakan/application/views/welcome_message.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 ?><!DOCTYPE html>
  <link href="<?php echo base_url('vendor/fontawesome-free/assets/css/all.min.css');?>" rel="stylesheet" type="text/css">
-  <link href="<?php('https://fonts.googleapis.com/assets/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');?>" rel="stylesheet">
+  <link href="<?php echo base_url('https://fonts.googleapis.com/assets/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');?>" rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="<?php echo base_url('assets/css/sb-admin-2.min.css');?>"rel="stylesheet">
