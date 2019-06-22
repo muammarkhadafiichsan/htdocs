828c343dadd1d15458893cf440f7d2eb440b0072
ismu.ubaid <ismu.ubaid@gmail.com>
2019-06-22 17:15:48 +0700

    proses

--- android/terbaru/app/src/main/java/com/jemberdeveloper/slidingtab/rest/ApiClient.java
+++ android/terbaru/app/src/main/java/com/jemberdeveloper/slidingtab/rest/ApiClient.java
@@ -7,15 +7,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-	public static final String BASE_URL = "http://192.168.1.17/rest_api/index.php/";
-	private static Retrofit retrofit = null;
+	public static final String base_url = "http://192.168.43.2/rest_api/index.php/api/";
+
+	private static Retrofit retrofit;
+
 	public static Retrofit getClient() {
-		if (retrofit==null) {
+		if (retrofit == null) {
 			retrofit = new Retrofit.Builder()
-					.baseUrl(BASE_URL)
+					.baseUrl(base_url)
 					.addConverterFactory(GsonConverterFactory.create())
 					.build();
 		}
+
 		return retrofit;
 	}
 }
