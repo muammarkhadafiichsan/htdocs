3304589b6fcaddde53e89e3dae3919422f476b5f
adhe <tnils600@gmail.com>
2019-06-22 16:43:18 +0700

    list product

--- wpu-login/application/views/templates/siderbar.php
+++ wpu-login/application/views/templates/siderbar.php
@@ -20,7 +20,7 @@
 
      <!-- Nav Item - Dashboard -->
      <li class="nav-item">
-         <a class="nav-link" href="index.html">
+         <a class="nav-link" href="<?php echo site_url('user') ?>">
              <i class="fas fa-fw fa-tachometer-alt"></i>
              <span>Dashboard</span></a>
      </li>
@@ -28,6 +28,19 @@
      <!-- Divider -->
      <hr class="sidebar-divider">
 
+     </li>
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'products' ? 'active': '' ?>">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-boxes"></i>
+            <span>Products</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" href="<?php echo site_url('admin/products/add') ?>">New Product</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/products') ?>">List Product</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/products/sms') ?>">Kirim SMS</a>
+        </div>
+    </li>
      <!-- Heading -->
      <div class="sidebar-heading">
          User
