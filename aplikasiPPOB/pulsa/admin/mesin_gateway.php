e8bbd76fb13f6c8a76a09199ad33cfc2c7082c3e
muammarkhadafiichsan <47959678+muammarkhadafiichsan@users.noreply.github.com>
2019-06-30 09:29:47 +0700

    gateway pulsa web

---
+++ aplikasiPPOB/pulsa/admin/mesin_gateway.php
@@ -0,0 +1,17 @@
+<?php
+
+$data = array("no" => "081295497313", "pesan" => "SMS dari PHP");
+$data_string = json_encode($data);
+
+$ch = curl_init('http://192.168.43.1:8000');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt(
+    $ch,
+    CURLOPT_HTTPHEADER,
+    array(
+        'Content-Length: ' . strlen($data_string)
+    )
+);
+$result = curl_exec($ch);
