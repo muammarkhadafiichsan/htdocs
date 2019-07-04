4ff4a19e75179897c55c0f94b6db65da66d79b9f
adhe <tnils600@gmail.com>
2019-07-05 00:55:28 +0700

    proses

--- aplikasiPPOB/pulsa3/application/controllers/User.php
+++ aplikasiPPOB/pulsa3/application/controllers/User.php
@@ -5,7 +5,7 @@ class User extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'Admin Dinas Peternakan';
+        $data['title'] = 'Admin Pulsa';
         $data['user'] = $this->db->get_where('user', ['email'
         => $this->session->userdata('email')])->row_array();
         //echo 'selamat datang ' . $data['user']['name'];
--- aplikasiPPOB/pulsa3/application/views/templates/user/siderbar.php
+++ aplikasiPPOB/pulsa3/application/views/templates/user/siderbar.php
@@ -6,7 +6,7 @@
          <div class="sidebar-brand-icon rotate-n-15">
              <i class="fas fa-code"></i>
          </div>
-         <div class="sidebar-brand-text mx-3">Dinas Peternakan
+         <div class="sidebar-brand-text mx-3">Juragan Pulsa
 
          </div>
      </a>
